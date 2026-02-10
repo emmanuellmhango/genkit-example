28e81fcd3577a415c097cacbd6e22998224e001f
emmanuel mahngo <emmanuellmhango@gmail.com>
2026-02-10 20:41:21 +0200

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import recipesRouter from './routes/recipes.js';
 
 const app = express();
 
+// Enable CORS for all origins (dev + prod)
+app.use(cors());
+
+// Parse JSON
 app.use(express.json());
 
+// Routes
 app.get('/', (_, res) => {
   res.json({ status: 'OK', service: 'Genkit Express API' });
 });
-
 app.use('/recipes', recipesRouter);
 
 export default app;
