a3983e60e55810520dd6049f619cedb1dad946c8
emmanuel mahngo <emmanuellmhango@gmail.com>
2026-02-10 19:55:02 +0200

    Updated config

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import recipesRouter from './routes/recipes.js';
+const app = express();
+app.use(express.json());
+app.get('/', (_, res) => {
+    res.json({ status: 'OK', service: 'Genkit Express API' });
+});
+app.use('/recipes', recipesRouter);
+export default app;
+//# sourceMappingURL=app.js.map
